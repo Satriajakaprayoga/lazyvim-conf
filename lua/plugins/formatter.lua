c07870beee875e3407ce3ea1a830a1d1e8b677c5
bomsiwor <bomsiwor@gmail.com>
2025-05-20 11:15:34 +0700

    add copilot to extras

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -36,18 +36,18 @@ return {
           stdin = false,
         },
 
-        -- pint = {
-        --   meta = {
-        --     url = "https://github.com/laravel/pint",
-        --     description = "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
-        --   },
-        --   command = util.find_executable({
-        --     vim.fn.stdpath("data") .. "/mason/bin/pint",
-        --     "vendor/bin/pint",
-        --   }, "pint"),
-        --   args = { "$FILENAME" },
-        --   stdin = false,
-        -- },
+        pint = {
+          meta = {
+            url = "https://github.com/laravel/pint",
+            description = "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
+          },
+          command = util.find_executable({
+            vim.fn.stdpath("data") .. "/mason/bin/pint",
+            "vendor/bin/pint",
+          }, "pint"),
+          args = { "$FILENAME" },
+          stdin = false,
+        },
       },
     },
   },
